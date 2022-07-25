b9e5727c8861cbb5992e65ae4cb63527aa41452e
Junior Rantila <junior.rantila@gmail.com>
2022-07-25 08:39:22 +0200

    JR: Add usize, isize, intptr_t and uintptr_t

--- JR/Types.h
+++ JR/Types.h
@@ -17,3 +17,15 @@ using f128 = long double;
 
 using string_literal = char const* nonnull const&&;
 using c_string = char const*;
+
+using usize = u64;
+using isize = i64;
+
+#ifdef __unix__
+#include <unistd.h>
+#else
+using intptr_t = i64;
+using uintptr_t = u64;
+static_assert(sizeof(intptr_t) == sizeof(void*));
+static_assert(sizeof(uintptr_t) == sizeof(void*));
+#endif
