49c5c9519c9e945ed35f80887033e28b1a6ee2b2
Junior Rantila <junior.rantila@gmail.com>
2022-07-27 21:04:44 +0200

    JR: Change 64 bit ints from long long to long

--- JR/Types.h
+++ JR/Types.h
@@ -4,12 +4,12 @@
 using i8 = char;
 using i16 = short;
 using i32 = int;
-using i64 = long long;
+using i64 = long;
 
 using u8 = unsigned char;
 using u16 = unsigned short;
 using u32 = unsigned int;
-using u64 = unsigned long long;
+using u64 = unsigned long;
 
 using f32 = float;
 using f64 = double;
