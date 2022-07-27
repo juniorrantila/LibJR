2d49ab5f67940ddbf6bd0272ad4c2d64b088934d
Junior Rantila <junior.rantila@gmail.com>
2022-07-27 21:05:34 +0200

    JR: Assert int and float type sizes

--- JR/Types.h
+++ JR/Types.h
@@ -5,15 +5,26 @@ using i8 = char;
 using i16 = short;
 using i32 = int;
 using i64 = long;
+static_assert(sizeof(i8) == 1);
+static_assert(sizeof(i16) == 2);
+static_assert(sizeof(i32) == 4);
+static_assert(sizeof(i64) == 8);
 
 using u8 = unsigned char;
 using u16 = unsigned short;
 using u32 = unsigned int;
 using u64 = unsigned long;
+static_assert(sizeof(u8) == 1);
+static_assert(sizeof(u16) == 2);
+static_assert(sizeof(u32) == 4);
+static_assert(sizeof(u64) == 8);
 
 using f32 = float;
 using f64 = double;
 using f128 = long double;
+static_assert(sizeof(f32) == 4);
+static_assert(sizeof(f64) == 8);
+static_assert(sizeof(f128) == 16);
 
 using string_literal = char const* nonnull const&&;
 using c_string = char const*;
